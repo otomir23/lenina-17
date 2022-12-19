e93e4f4e266486c17c15b977f5aee5f7582fb49d
Damir Modyarov <otomir@yandex.ru>
2022-12-19 19:02:46 +0300

    Добавил второй кусочек картинки

--- quest.py
+++ quest.py
@@ -23,29 +23,16 @@ class QuestRoom(Room):
     def apply_objects(self):
         """Добавление объектов"""
 
-        # Загружаем картинку чая и меняем ей размер
+        # Загружаем картинки
+        paper_image = load_image("paper.png")
+        paper_image = pygame.transform.scale(paper_image, (24, 24))
+
         tea_image = load_image("tea.png")
         tea_image = pygame.transform.scale(tea_image, (100, 67))
 
-        # Создаем объект чая и привязываем к нему функцию по клику
-        tea_object = RoomObject(tea_image, (400, 320))
-        tea_object.click_hook = self.click_tea
-
-        # Добавляем объект в комнату на стену 0 (переднюю)
-        self.add_objects(tea_object, wall=0)
-
-        # Загружаем картинку чашки и меняем ей размер
         cup_image = load_image("cup.png")
         cup_image = pygame.transform.scale(cup_image, (100, 72))
 
-        # Создаем объект чашки и привязываем к нему функцию по клику
-        cup_object = RoomObject(cup_image, (400, 370))
-        cup_object.click_hook = self.click_cup
-
-        # Добавляем объект в комнату на стену 3 по часовой стрелке (левую)
-        self.add_objects(cup_object, wall=3)
-
-        # Загружаем картинки книг и меняем их размер
         book1_image = load_image("alyonka.png")
         book1_image = pygame.transform.scale(book1_image, (25, 67))
 
@@ -55,13 +42,40 @@ class QuestRoom(Room):
         book3_image = load_image("alyonka.png")
         book3_image = pygame.transform.scale(book3_image, (33, 65))
 
+        matryoshka_top_image = load_image("matryoshka_top.png")
+        matryoshka_top_image = pygame.transform.scale(matryoshka_top_image, (32, 32))
+
+        matryoshka_bottom_image = load_image("matryoshka_bottom.png")
+        matryoshka_bottom_image = pygame.transform.scale(matryoshka_bottom_image, (32, 32))
+
+        # Создаем объект чая и привязываем к нему функцию по клику
+        tea_object = RoomObject(tea_image, (400, 320))
+        tea_object.click_hook = self.click_tea
+
+        # Добавляем объект в комнату на стену 0 (переднюю)
+        self.add_objects(tea_object, wall=0)
+
+        # Создаем объект чашки и привязываем к нему функцию по клику
+        cup_object = RoomObject(cup_image, (400, 370))
+        cup_object.click_hook = self.click_cup
+
         # Создаем объекты книг
         book1_object = DraggableObject(book1_image, (250, 280))
         book2_object = DraggableObject(book2_image, (300, 280))
         book3_object = DraggableObject(book3_image, (360, 280))
 
+        # Создаем объекты матрешки
+        matryoshka_top = RoomObject(matryoshka_top_image, (400, 185))
+        matryoshka_bottom = RoomObject(matryoshka_bottom_image, (400, 185 + matryoshka_top_image.get_height()))
+        paper_piece2 = RoomObject(paper_image, (400, 185))
+
+        matryoshka_top.click_hook = self.click_matryoshka_top
+        matryoshka_top.update_hook = self.update_matryoshka_top
+        paper_piece2.update_hook = self.update_piece
+        paper_piece2.click_hook = self.get_piece_click_handler(2)
+
         # Добавляем объекты в комнату на стену 3 по часовой стрелке (левую)
-        self.add_objects(book1_object, book2_object, book3_object, wall=3)
+        self.add_objects(cup_object, book1_object, book2_object, book3_object, paper_piece2, matryoshka_top, matryoshka_bottom, wall=3)
 
         # Создаем лампу
         lamp = RoomObject(load_image("lamp_off.png"), (400, 300))
@@ -118,6 +132,55 @@ class QuestRoom(Room):
             self.inventory.add(Item("piece_1", "Кусочек картинки", load_image("paper.png")))
             obj.storage['piece_taken'] = True
             obj.image = load_image("lamp_on_empty.png")
+
+    def click_matryoshka_top(self, obj, *_):
+        """Обработчик клика по верхней части матрёшки"""
+
+        # Если верхняя часть матрёшки открыта, то ничего не делаем
+        if 'opened' in obj.storage:
+            return
+
+        # Добавляем в хранилище объекта информацию о том, что матрёшка начала открытие
+        obj.storage['opened'] = False
+
+    def update_matryoshka_top(self, obj, dt):
+        """Обновление верхней части матрёшки"""
+
+        if 'opened' in obj.storage:
+            y_target = 100
+
+            # Если анимация не началась, начинаем её
+            if not obj.storage['opened']:
+                obj.rect.y -= 10
+            else:
+                obj.image = pygame.Surface((0, 0))
+
+            # Если верхняя часть матрёшки достигла цели, то сохраняем информацию о том, что она открыта
+            if obj.rect.y <= y_target:
+                obj.storage['opened'] = True
+
+    def get_piece_click_handler(self, piece: int):
+        """Возвращает обработчик клика по куску картинки"""
+
+        def click_piece(obj, *_):
+            # Если кусок картинки взят, то ничего не делаем
+            if 'taken' in obj.storage:
+                return
+
+            # Добавляем в хранилище объекта информацию о том, что кусок картинки взят
+            obj.storage['taken'] = True
+
+            # Добавляем кусок картинки в инвентарь
+            self.inventory.add(Item(f"piece_{piece}", "Кусочек картинки", load_image("paper.png")))
+
+        return click_piece
+
+    def update_piece(self, obj, dt):
+        """Обновление куска картинки"""
+
+        # Если кусок картинки брали, то удаляем его
+        if 'taken' in obj.storage:
+            obj.image = pygame.Surface((0, 0))
 
 
 class DraggableObject(RoomObject):
