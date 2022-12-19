dc7c7ddb47e7c23b71b11daecc591d8fb2f6e4a1
Damir Modyarov <otomir@yandex.ru>
2022-12-19 16:50:35 +0300

    Исправил работу перетаскиваемого объекта

--- quest.py
+++ quest.py
@@ -55,25 +55,15 @@ class QuestRoom(Room):
         book3_image = load_image("alyonka.png")
         book3_image = pygame.transform.scale(book1_image, (33, 65))
 
-        # Создаем объекты книг и привязываем к ним функцию по клику
-        book1_object = RoomObject(book1_image, (250, 280))
-        book1_object.click_hook = self.click_books(book1_image, (250, 280))
+        # Создаем объекты книг
+        book1_object = DraggableObject(book1_image, (250, 280))
+        book2_object = DraggableObject(book2_image, (300, 280))
+        book3_object = DraggableObject(book3_image, (360, 280))
 
-        book2_object = RoomObject(book2_image, (300, 280))
-        book2_object.click_hook = self.click_books(book2_image, (300, 280))
-
-        book3_object = RoomObject(book3_image, (360, 280))
-        book3_object.click_hook = self.click_books(book3_image, (360, 280))
-
-        # Добавляем объект в комнату на стену 3 по часовой стрелке (левую)
+        # Добавляем объекты в комнату на стену 3 по часовой стрелке (левую)
         self.add_objects(book1_object, wall=3)
-
         self.add_objects(book2_object, wall=3)
-
         self.add_objects(book3_object, wall=3)
-
-    def click_books(self, image, pos):
-        DraggableObject(image, pos)
 
     def click_tea(self, obj, *_):
         """Обработчик клика по чаю"""
@@ -116,10 +106,10 @@ class DraggableObject(RoomObject):
         self.pos = pos
         self.offset = None
 
-
     def update(self, delta_time: float):
-        # Если мышь нажата и не была нажата раньше
-        if pygame.mouse.get_pressed()[0] and self.offset is None:
+        # Если мышь нажата и не была нажата раньше, а также мышь над объектом
+        if pygame.mouse.get_pressed()[0] and self.offset is None\
+                and self.rect.collidepoint(pygame.mouse.get_pos()):
             # Сохраняем смещение относительно объекта
             self.offset = pygame.mouse.get_pos()[0] - self.pos[0], pygame.mouse.get_pos()[1] - self.pos[1]
 
@@ -139,5 +129,3 @@ class DraggableObject(RoomObject):
 
         # Вызываем метод родителя
         super().update(delta_time)
-
-
