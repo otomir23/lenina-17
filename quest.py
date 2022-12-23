b1faf3cb73006a9cd338487c9d4844a7493190e6
WitchElizabeth <valya.grekhova@gmail.com>
2022-12-23 21:56:19 +0300

    Наконец-то завершена головоломка с книгами. Остались только спрайты книг, заменить их на тестовые и головоломка будет считаться доведённой до конца.

--- quest.py
+++ quest.py
@@ -4,6 +4,8 @@ from inventory import Item
 from room import Room, RoomObject
 from ui import apply_ui
 from utils import load_image
+from typing import Tuple, Union
+from pygame.surface import SurfaceType
 
 
 class QuestRoom(Room):
@@ -42,6 +44,9 @@ class QuestRoom(Room):
         book3_image = load_image("alyonka.png")
         book3_image = pygame.transform.scale(book3_image, (33, 65))
 
+        solved_book_image = load_image("alyonka_solved.png")
+        solved_book_image = pygame.transform.scale(solved_book_image, (150, 80))
+
         matryoshka_top_image = load_image("matryoshka_top.png")
         matryoshka_top_image = pygame.transform.scale(matryoshka_top_image, (32, 32))
 
@@ -79,10 +84,12 @@ class QuestRoom(Room):
         cup_object = RoomObject(cup_image, (400, 370))
         cup_object.click_hook = self.click_cup
 
-        # Создаем объекты книг
-        book1_object = DraggableObject(book1_image, (250, 280))
-        book2_object = DraggableObject(book2_image, (300, 280))
-        book3_object = DraggableObject(book3_image, (360, 280))
+        # Создаем объект книг
+        book_puzzle = BookPuzzle([
+            (1, book1_image),
+            (2, book2_image),
+            (0, book3_image),
+        ], solved_book_image, (260, 280))
 
         # Создаем объекты матрешки
         matryoshka_top = RoomObject(matryoshka_top_image, (400, 185))
@@ -94,8 +101,7 @@ class QuestRoom(Room):
         paper_piece2.update_hook = self.update_piece
         paper_piece2.click_hook = self.get_piece_click_handler(2)
 
-        # Добавляем объекты в комнату на стену 3 по часовой стрелке (левую)
-        self.add_objects(cup_object, book1_object, book2_object, book3_object, paper_piece2, matryoshka_top, matryoshka_bottom, wall=3)
+        self.add_objects(cup_object, book_puzzle, paper_piece2, matryoshka_top, matryoshka_bottom, wall=3)
 
     def click_tea(self, obj, *_):
         """Обработчик клика по чаю"""
@@ -196,38 +202,106 @@ class QuestRoom(Room):
             obj.image = pygame.Surface((0, 0))
 
 
-class DraggableObject(RoomObject):
-    """Объект, который можно перетаскивать"""
+class BookPuzzle(RoomObject):
+    """Головоломка с книгами"""
 
-    def __init__(self, image, pos):
+    def __init__(
+            self,
+            books: list[Tuple[int, Union[pygame.Surface, SurfaceType]]],
+            solved_image: pygame.Surface, pos: Tuple[int, int]
+    ):
         """Создание объекта"""
 
-        super().__init__(image, pos)
+        # Находим ширину и высоту объекта
+        width = sum(book[1].get_width() for book in books)
+        height = max(book[1].get_height() for book in books)
 
-        # Сохраняем начальную позицию
-        self.pos = pos
-        self.offset = None
+        super().__init__(pygame.Surface((width, height), pygame.SRCALPHA, 32), pos)
+
+        # Инициализируем книги
+        x = 0
+        self.books = pygame.sprite.Group()
+        for book in books:
+            # Создаём спрайт книги
+            book_sprite = pygame.sprite.Sprite()
+            book_sprite.right_x = book[0]
+            book_sprite.image = book[1]
+            book_sprite.rect = book_sprite.image.get_rect()
+            book_sprite.rect.x = x
+            book_sprite.rect.y = self.rect.height - book_sprite.rect.height
+
+            # Добавляем книгу в группу
+            self.books.add(book_sprite)
+            x += book_sprite.rect.width
+
+        # Сохраняем информацию о том, что книги не перемещались
+        self.grabbed_book = None
+        self.grabbed_book_offset = None
+
+        # Сохраняем изображение, которое будет отображаться, когда книги будут расставлены правильно
+        self.solved_image = solved_image
+
+    def get_book_key(self, book: pygame.sprite.Sprite):
+        """Получение ключа книги для сортировки"""
+
+        if book is self.grabbed_book:
+            return pygame.mouse.get_pos()[0] - self.rect.x - self.grabbed_book_offset
+        return book.rect.x
 
     def update(self, delta_time: float):
-        # Если мышь нажата и не была нажата раньше, а также мышь над объектом
-        if pygame.mouse.get_pressed()[0] and self.offset is None\
-                and self.rect.collidepoint(pygame.mouse.get_pos()):
-            # Сохраняем смещение относительно объекта
-            self.offset = pygame.mouse.get_pos()[0] - self.pos[0], pygame.mouse.get_pos()[1] - self.pos[1]
+        # Если головоломка не решена, то обрабатываем перемещение книг
+        if 'solved' not in self.storage or not self.storage['solved']:
+            self.handle_drag()
+            sorted_books = sorted(self.books, key=self.get_book_key)
 
-        # Если мышь отпущена
-        if not pygame.mouse.get_pressed()[0]:
-            # Сбрасываем смещение
-            self.offset = None
+            # Проверяем стоят ли книги на своих местах
+            if all(book.right_x == i for i, book in enumerate(sorted_books)):
+                self.storage['solved'] = True
+                self.grabbed_book = None
+                self.grabbed_book_offset = None
+                sorted_books[0].image = self.solved_image
 
-        # Если смещение не равно None
+            # Обновляем позиции книг на основе их порядка
+            x = 0
+            for book in sorted_books:
+                if book != self.grabbed_book:
+                    book.rect.x = x
+                x += book.rect.width
 
-        if self.offset is not None:
-            # Перемещаем объект на новую позицию
-            self.pos = pygame.mouse.get_pos()[0] - self.offset[0], pygame.mouse.get_pos()[1] - self.offset[1]
+        # Очищаем поверхность прозрачным цветом
+        self.image.fill((0, 0, 0, 0))
 
-        # Обновляем позицию объекта
-        self.rect = self.image.get_rect(center=self.pos)
+        # Отрисовываем книги
+        self.books.draw(self.image)
 
         # Вызываем метод родителя
         super().update(delta_time)
+
+    def handle_drag(self):
+        """Обработка перетаскивания объекта"""
+
+        # Если мышь нажата и не была нажата раньше
+        if pygame.mouse.get_pressed()[0] and self.grabbed_book is None:
+            # Получаем какую книгу мы хотим перетащить
+            for book in self.books:
+                if book.rect.collidepoint(
+                        (pygame.mouse.get_pos()[0] - self.rect.x, pygame.mouse.get_pos()[1] - self.rect.y)
+                ):
+                    self.grabbed_book = book
+                    self.grabbed_book_offset = pygame.mouse.get_pos()[0] - self.rect.x - book.rect.x
+                    break
+
+        # Если мышь отпущена
+        if not pygame.mouse.get_pressed()[0]:
+            # Сбрасываем информацию о том, что мы перетаскиваем книгу
+            self.grabbed_book = None
+
+        # Если книга перетаскивается
+        if self.grabbed_book is not None:
+            # Перемещаем книгу под мышь
+            target_x = pygame.mouse.get_pos()[0] - self.rect.x - self.grabbed_book_offset
+            self.grabbed_book.rect.x = target_x
+            if self.grabbed_book.rect.x < 0:
+                self.grabbed_book.rect.x = 0
+            if self.grabbed_book.rect.x > self.rect.width - self.grabbed_book.rect.width:
+                self.grabbed_book.rect.x = self.rect.width - self.grabbed_book.rect.width
